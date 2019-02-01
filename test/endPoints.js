a3cb12619700826ba28d5f89dfba118d8b4daa2b
Tim Vlasuk <timvlasuk@gmail.com>
2019-02-01 12:37:29 -0800

    finished mock testing for POST with good and bad example

--- test/endPoints.js
+++ test/endPoints.js
@@ -66,49 +66,97 @@ function mockData() {
     })
     // .then(function(post) {console.log(post)})
 }
+    /*set up GET tests */
+    describe('Get route', function() {
 
-describe('Get route', function() {
-
-    testHooks();
-    it('should return a list of restaurants', function() {
-         return chai.request(app)
-          .get('/posts')
-          .then(function(res) {
-            expect(res).to.have.status(200)
-            expect(res.body).to.be.an('array')
-            expect(res).to.be.an('object')
-            res.body.forEach(post => {
-                expect(post).to.have.property('author').that.is.a('string')
-                expect(post).to.have.property('title').that.is.a('string')
-                expect(post).to.have.property('content').that.is.a('string')
-
-            })
-            
-          })
-    })
-
-    it('should return a single post when provided author id in params', function() {
-
-        var postId
-
-        /*Get list to use one */
-        return chai.request(app)
-        .get('/posts')
-        .then(res => {
-            expect(res).to.have.status(200)
-            expect(res).to.be.an('object')
-            postId = res.body[0].id
+        testHooks();
+        it('should return a list of restaurants', function() {
             return chai.request(app)
-                .get(`/posts/${postId}`)
-                .then(res => {
-                    expect(res.body.id).to.equal(postId)
+            .get('/posts')
+            .then(function(res) {
+                expect(res).to.have.status(200)
+                expect(res.body).to.be.an('array')
+                expect(res).to.be.an('object')
+                res.body.forEach(post => {
+                    expect(post).to.have.property('author').that.is.a('string')
+                    expect(post).to.have.property('title').that.is.a('string')
+                    expect(post).to.have.property('content').that.is.a('string')
+
                 })
+                
+            })
         })
 
-        
-        
+        it('should return a single post when provided author id in params', function() {
+
+            var postId
+
+            /*Get list to use one */
+            return chai.request(app)
+            .get('/posts')
+            .then(res => {
+                expect(res).to.have.status(200)
+                expect(res).to.be.an('object')
+                postId = res.body[0].id
+                return chai.request(app)
+                    .get(`/posts/${postId}`)
+                    .then(res => {
+                        expect(res.body.id).to.equal(postId)
+                    })
+            })
+
+        })
 
     })
+    /*set up Post tests */
+    describe('Post route', function() {
+
+        testHooks();
+
+        const newPost = {
+            author: {
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName()
+            },
+            title: faker.lorem.words(),
+            content: faker.lorem.paragraph()
+        }
+
+        const badPost = {
+            author: {
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName()
+            },
+            title: faker.lorem.words()
+        }
+
+        it('should create a new post', function() {
+            return chai.request(app)
+            .post('/posts')
+            .send(newPost)
+            .then(res => {
+                expect(res).to.have.status(201)
+                expect(res).to.be.an('object')
+                expect(res.body).to.be.an('object')
+                expect(res.body.author).to.equal(`${newPost.author.firstName}` + ' ' + `${newPost.author.lastName}`)
+                expect(res.body.title).to.equal(newPost.title)
+                expect(res.body.content).to.equal(newPost.content)
+                console.log()        
+            })
+        })
+
+        it('should fail if request is missing data', function() {
+            return chai.request(app)
+            .post('/posts')
+            .send(badPost)
+            .then(res => {
+                expect(res).to.have.status(400)
+            })
+        })
+
+        describe('Update/PUT routes', function() {
+            
+        })
 
 
 })
